4255c87f7771c1812db79c788614b1ab92ec022a
devanshbatra04 <devanshbatra04@gmail.com>
2018-05-26 15:30:30 +0530

    Added mock homepage

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-v
+var express = require('express');
+
+var app = express();
+app.set('view engine', 'ejs');
+
+app.get("/", function(req,res){
+    res.render("home");
+});
+
+var port = process.env.PORT || 5000;
+
+app.listen(port, function(){
+    console.log("Running on port " + port);
+});
